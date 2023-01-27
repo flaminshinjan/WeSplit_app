f2d76d0bf8c04683531eea7949065832a7511da6
flaminshinjan <srikpatra.2008@gmail.com>
2023-01-27 06:45:47 +0530

    Complete App

---
+++ ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Shinjan Patra on 20/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        ZStack {
+            NavigationView{
+                Form{
+                    Section {
+                        TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                            .keyboardType(.decimalPad)
+                        Picker("Number of people", selection: $numberOfPeople) {
+                                ForEach(2 ..< 100) {
+                                    Text("\($0) people")
+                                }
+                        }
+                    }
+                    Section{
+                        Picker("Tip percentage", selection: $tipPercentage) {
+                            ForEach(tipPercentages, id: \.self) {
+                                Text($0, format: .percent)
+                            }
+                        }.pickerStyle(.segmented)
+                    }header:{
+                        Text("How much tip do you want to leave?")
+                    }
+                
+                    Section {
+                        Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    }
+                }.navigationTitle("WeSplit")
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Shinjan Patra on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
